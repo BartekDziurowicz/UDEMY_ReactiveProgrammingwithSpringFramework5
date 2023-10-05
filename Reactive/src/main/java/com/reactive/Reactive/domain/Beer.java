362ad8c9e49a1f0f4456c45d59079db52ebc2cad
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2023-10-05 16:31:27 +0200

    47 maven dependencies

--- Reactive/src/main/java/com/reactive/Reactive/domain/Beer.java
+++ Reactive/src/main/java/com/reactive/Reactive/domain/Beer.java
@@ -2,14 +2,10 @@ package com.reactive.Reactive.domain;
 
 
 import com.reactive.Reactive.web.model.BeerStyleEnum;
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
@@ -22,17 +18,10 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Entity
 public class Beer {
 
-    @Id
-    @GeneratedValue(generator = "UUID")
-    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-//    @Type(type="org.hibernate.type.UUIDCharType")
-    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
     private UUID id;
 
-    @Version
     private Long version;
 
     private String beerName;
@@ -42,10 +31,7 @@ public class Beer {
     private Integer quantityOnHand;
     private BigDecimal price;
 
-    @CreationTimestamp
-    @Column(updatable = false)
     private Timestamp createdDate;
 
-    @UpdateTimestamp
     private Timestamp lastModifiedDate;
 }
